7396ab4d7e14e8056a4075c54a05211af107efc0
Rahul <heyyrj98@gmail.com>
2024-10-12 21:54:07 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,18 +2,40 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
+require("dotenv").config();
+
 const ownersRouter = require("./routes/ownersRouter");
-const usersRouter = require("./routes/usersRouter");
 const productsRouter = require("./routes/productsRouter");
+const usersRouter = require("./routes/usersRouter");
+const indexRouter = require("./routes/index");
 
 const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
+app.use(
+    expressSession({
+        secret: process.env.EXPRESS_SESSION_SECRET || "default_secret",  // Provide a fallback for the secret
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 60000 }  // Optional: Set cookie expiration time
+    })
+);
+app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+app.use(function(req, res, next) {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use("/", indexRouter);
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const upload = require("../config/multer-config");
+const productModel = require("../models/product-model");
 
-router.get("/", function (req, res) {
-    res.send("heyy product is working");
+router.get("/create", function (req, res) {
+    res.render("createproducts");  // Render the form
+});
+
+router.post("/create", upload.single("image"), async function (req, res) {
+    try {
+        let { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
+
+        let product = await productModel.create({
+        image: req.file.buffer,
+        name,
+        price,
+        discount,
+        bgcolor,
+        panelcolor,
+        textcolor,
+        });
+
+        req.flash("success", "Product created successfully");
+        res.redirect("/owners/admin");
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
 });
 
 module.exports = router;
